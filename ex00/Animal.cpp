951c2ef1386831eb40f5806c5bfe465cd2ee998b
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-04-04 12:00:29 +0000

    implement Animal class with constructors, assignment operator, destructor, and getter/setter methods

--- ex00/Animal.cpp
+++ ex00/Animal.cpp
@@ -6,7 +6,47 @@
 /*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 22:19:25 by macbookpro        #+#    #+#             */
-/*   Updated: 2025/04/03 22:19:26 by macbookpro       ###   ########.fr       */
+/*   Updated: 2025/04/04 12:00:14 by macbookpro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Animal.hpp"
+#include <iostream>
+
+Animal::Animal() : type("Animal") {
+    std::cout << "Animal default constructor called" << std::endl;
+}
+
+Animal::Animal(const std::string& name) : type(name) {
+    std::cout << "Animal parameterized constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal& other) : type(other.type) {
+    std::cout << "Animal copy constructor called" << std::endl;
+}
+
+Animal& Animal::operator=(const Animal& other) {
+    if (this != &other) {
+        this->type = other.type;
+    }
+    std::cout << "Animal assignment operator called" << std::endl;
+    return *this;
+}
+
+Animal::~Animal() {
+    std::cout << "Animal destructor called" << std::endl;
+}
+
+const std::string& Animal::getName() const {
+    return this->type;
+}
+
+void Animal::setName(const std::string& name) {
+    this->type = name;
+}
+
+std::string Animal::getType()
+{
+    return (this->getType());   
+}
 
