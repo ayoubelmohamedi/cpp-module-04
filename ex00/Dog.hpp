ba7b893b581b872701ebc7bf13e34415690c6740
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-04-04 12:33:32 +0000

    add type member to Dog class and override makeSound method

--- ex00/Dog.hpp
+++ ex00/Dog.hpp
@@ -6,7 +6,7 @@
 /*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 22:27:21 by macbookpro        #+#    #+#             */
-/*   Updated: 2025/04/03 22:27:24 by macbookpro       ###   ########.fr       */
+/*   Updated: 2025/04/04 12:33:20 by macbookpro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,20 @@
 #define DOG_HPP
 
 #include "Animal.hpp"
+#include <string>
 
 class Dog : public Animal {
+private:
+    std::string type;
 public:
     Dog();
     Dog(const Dog& other);
     Dog& operator=(const Dog& other);
     ~Dog();
 
-    void makeSound() const;
+    void makeSound() const override;
+    void getType();
+
 };
 
 #endif
