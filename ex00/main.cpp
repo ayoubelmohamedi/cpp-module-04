09248dc10ded1241b202e17ce55f605d44187cfa
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-04-04 12:51:53 +0000

    add main function to demonstrate Animal, Dog, and Cat classes

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,23 @@
 /*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 22:19:23 by macbookpro        #+#    #+#             */
-/*   Updated: 2025/04/03 22:19:24 by macbookpro       ###   ########.fr       */
+/*   Updated: 2025/04/04 12:16:41 by macbookpro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include <iostream>
+
+int main()
+{
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+    return 0;
+}
