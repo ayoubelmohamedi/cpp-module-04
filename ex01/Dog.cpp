7caa9e6d769be45b4467287bb51f5ee4e4b72ae5
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-04-05 09:16:44 +0000

    add: implement Brain management in Dog class with deep copy and destructor; update makeSound method

--- ex01/Dog.cpp
+++ ex01/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 22:27:29 by macbookpro        #+#    #+#             */
-/*   Updated: 2025/04/04 12:56:50 by macbookpro       ###   ########.fr       */
+/*   Updated: 2025/04/05 09:15:15 by macbookpro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,29 @@
 
 Dog::Dog() : Animal("Dog") {
     std::cout << "Dog default constructor called" << std::endl;
-    this->type = "Dog";
+    this->brain = new Brain(); // Allocate Brain
 }
 
 Dog::Dog(const Dog& other) : Animal(other) {
     std::cout << "Dog copy constructor called" << std::endl;
+    this->brain = new Brain(*other.brain); // Deep copy Brain
 }
 
 Dog& Dog::operator=(const Dog& other) {
-    if (this != &other) {
-        Animal::operator=(other);
-    }
     std::cout << "Dog assignment operator called" << std::endl;
+    if (this != &other) {
+        Animal::operator=(other); // Call base class assignment operator
+        delete this->brain; // Delete old Brain
+        this->brain = new Brain(*other.brain); // Deep copy new Brain
+    }
     return *this;
 }
 
 Dog::~Dog() {
     std::cout << "Dog destructor called" << std::endl;
+    delete this->brain; // Deallocate Brain
 }
 
 void Dog::makeSound() const {
-    std::cout << "Woof!" << std::endl;
+    std::cout << "Woof Woof" << std::endl;
 }
--- ex01/Dog.hpp
+++ ex01/Dog.hpp
@@ -6,27 +6,27 @@
 /*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 22:27:21 by macbookpro        #+#    #+#             */
-/*   Updated: 2025/04/04 13:28:51 by macbookpro       ###   ########.fr       */
+/*   Updated: 2025/04/05 09:15:15 by macbookpro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef DOG_HPP
 #define DOG_HPP
 
 #include "Animal.hpp"
-#include <string>
+#include "Brain.hpp" // Add include for Brain
 
 class Dog : public Animal {
 private:
-    std::string type;
+    Brain* brain; // Add private Brain pointer
+
 public:
     Dog();
     Dog(const Dog& other);
     Dog& operator=(const Dog& other);
-    ~Dog();
+    virtual ~Dog(); // Make destructor virtual
 
-    void makeSound() const;
+    virtual void makeSound() const;
 };
 
 #endif
