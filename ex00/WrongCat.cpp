e66cd1fe6cd5074bee633eaf54187e92a1f0d427
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-05-02 19:07:44 +0100

    WrongCat : derived class implimentation with header

---
+++ ex00/WrongCat.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 18:59:44 by ael-moha          #+#    #+#             */
+/*   Updated: 2025/05/02 19:04:39 by ael-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat() : WrongAnimal("WrongCat") {
+    std::cout << "WrongCat default constructor called" << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat& other) : WrongAnimal(other) {
+    std::cout << "WrongCat copy constructor called" << std::endl;
+}
+
+WrongCat& WrongCat::operator=(const WrongCat& other) {
+    if (this != &other) {
+        WrongAnimal::operator=(other);
+    }
+    std::cout << "WrongCat assignment operator called" << std::endl;
+    return *this;
+}
+
+WrongCat::~WrongCat() {
+    std::cout << "WrongCat destructor called" << std::endl;
+}
+
+void WrongCat::makeSound() const {
+    std::cout << "Wrong Meow!" << std::endl;
+}
+
+
---
+++ ex00/WrongCat.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 18:59:48 by ael-moha          #+#    #+#             */
+/*   Updated: 2025/05/02 19:00:49 by ael-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef WRONGCAT_HPP
+#define WRONGCAT_HPP
+
+#include "WrongAnimal.hpp"
+
+class WrongCat : public WrongAnimal {
+public:
+    WrongCat();
+    WrongCat(const WrongCat& other);
+    WrongCat& operator=(const WrongCat& other);
+    ~WrongCat();
+
+    void makeSound() const;
+};
+
+#endif
