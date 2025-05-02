8597cb40469047fd64281980fae5a86c5c089a4d
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-05-02 18:52:12 +0100

    move code to apppropriate header file

--- ex00/WrongAnimal.hpp
+++ ex00/WrongAnimal.hpp
@@ -1,12 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   WrongAnimal.cpp                                    :+:      :+:    :+:   */
+/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/02 18:46:55 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/05/02 18:51:46 by ael-moha         ###   ########.fr       */
+/*   Created: 2025/05/02 18:51:47 by ael-moha          #+#    #+#             */
+/*   Updated: 2025/05/02 18:51:51 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "WrongAnimal.hpp"
+#include <iostream>
+
+class WrongAnimal {
+protected:
+    std::string type;
+public:
+    WrongAnimal();
+    WrongAnimal(const WrongAnimal& other);
+    WrongAnimal& operator=(const WrongAnimal& other);
+    virtual ~WrongAnimal();
+
+    void makeSound() const;
+    std::string getType() const;
+};
