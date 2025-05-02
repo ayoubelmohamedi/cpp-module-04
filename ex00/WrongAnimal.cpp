2f6ebf7f52c4e6348b8277e622fffa4117755330
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-05-02 18:58:53 +0100

    impliment WrongAnimal class

--- ex00/WrongAnimal.cpp
+++ ex00/WrongAnimal.cpp
@@ -6,7 +6,39 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 18:46:55 by ael-moha          #+#    #+#             */
-/*   Updated: 2025/05/02 18:51:46 by ael-moha         ###   ########.fr       */
+/*   Updated: 2025/05/02 18:58:10 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+#include "WrongAnimal.hpp"
+
+
+WrongAnimal::WrongAnimal() : type("WrongAnimal") {
+    std::cout << "WrongAnimal default constructor called" << std::endl;
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal& other) : type(other.type) {
+    std::cout << "WrongAnimal copy constructor called" << std::endl;
+}
+
+WrongAnimal& WrongAnimal::operator=(const WrongAnimal& other) {
+    if (this != &other) {
+        this->type = other.type;
+    }
+    std::cout << "WrongAnimal assignment operator called" << std::endl;
+    return *this;
+}
+
+WrongAnimal::~WrongAnimal() {
+    std::cout << "WrongAnimal destructor called" << std::endl;
+}
+
+void WrongAnimal::makeSound() const {
+    std::cout << "WrongAnimal, no sound!" << std::endl;
+}
+
+std::string WrongAnimal::getType() const {
+    return (this->type);
+}
 
